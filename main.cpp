44267b36fe518e0e1d0f5cf93a4d7694670e093b
Dhruv Batra <dhruvbatra2@gmail.com>
2024-09-11 18:12:34 -0400

    clean up code/comments

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main() {
     }
 
     // error if input > 1000 characters
-    if (std::cin.fail() || input[0] == '\n') {
+    if (std::cin.fail()) {
       std::cerr << "error: input exceeds 1000 characters" << std::endl;
       std::cin.clear();
       std::cin.ignore(INT_MAX, '\n');
@@ -39,19 +39,7 @@ int main() {
         continue;
     }
 
-    //TESTING
-    //  int b = 0;
-    // std::cout << "\n\n\n\n\n" << std::endl;
-    //  while (tokens[b] != nullptr) {
-    //      std::cout << tokens[b] << std::endl;
-    //      ++b;
-    //  }
-    // std::cout << "\n\n\n\n\n" << std::endl;
-
-
     runCommand(const_cast<const char **>(tokens));
-
-
 
     // free allocated memory
     int i = 0;
@@ -67,20 +55,18 @@ int main() {
 
 // clean up input and output pointer to token array
 char **parseTokens(char input[]) {
-  // trim leading and trailing spaces
+  //check if just enter was pressed which results in just null terminating char
   char *start = input;
   char *end = input + strlen(input)-1;
-
-  if(*start=='\0'){ //check if just enter was pressed which results in just null terminating char
+  if(*start=='\0'){ 
     return nullptr;
   }
-
-  // advance leading edge to remove spaces
-  while (isspace(*start)) {
+  
+  // trim leading and trailing spaces
+  while (isspace(*start)) { // advance leading edge to remove spaces
     start++;
   }
-  // retract trailing edge to remove spaces
-  while (isspace(*end)) {
+  while (isspace(*end)) { // retract trailing edge to remove spaces
     end--;
   }
   *(end + 1) = '\0'; // replace null terminating char if the end was moved
@@ -100,15 +86,15 @@ char **parseTokens(char input[]) {
   char **args = new char *[100];
   int arg_count = 0;
 
+  //parse input and seperate into tokens
   while (*runner != '\0') {
-    // single/double quotes count as one token
-    if (*runner == '\'') {
+    if (*runner == '\'') { //if single quote encountered
       prev_space = false;
       if (!s_quote && !d_quote && !norm) { // start token capture if no one else has
         start_token = runner+1;
         s_quote = true;
         s_count++;
-      } else if (d_quote) { // if double quote going on and single is encountered
+      } else if (d_quote) { // if double quote going on and single is encountered then proceed
         runner++;
         continue;
       } else if (norm && !s_quote) { // case where space is encountered before opening quote then change state to quote
@@ -128,7 +114,7 @@ char **parseTokens(char input[]) {
         start_token = runner+1;
         d_count++;
         d_quote = true;
-      } else if (s_quote) { // if single quote is going on and double is encountered record and proceed
+      } else if (s_quote) { // if single quote is going on and double is encountered then proceed
         runner++;
         continue;
       } else if (norm && !d_quote) { // case where space is encountered before opening quote then change state to quote
@@ -142,8 +128,7 @@ char **parseTokens(char input[]) {
         addToken(start_token, runner, args, arg_count);
       }
     } else if (*runner == ' ') { // normal case of space delimted word
-      if (!s_quote && !d_quote &&
-          !norm) { // start token capture if no one else has
+      if (!s_quote && !d_quote && !norm) { // start token capture if no one else has
         start_token = runner + 1;
         prev_space = true;
         norm = true;
@@ -157,13 +142,11 @@ char **parseTokens(char input[]) {
           continue;
         }
         addToken(start_token, runner, args, arg_count);
-        // another space has been encountered
-        norm = false;
+        norm = false; 
         start_token = runner + 1;
       }
-      // just finished handling a space
-      prev_space = true;
-    } else { // just a character
+      prev_space = true; // just finished handling a space
+    } else { // other character encountered
       prev_space = false;
       if (!s_quote && !d_quote && !norm) { // start token capture if no one else has
         start_token = runner;
@@ -200,15 +183,15 @@ void runCommand(const char *args[]) {
 
   int status=0;
   pid_t pid;
-  char *arg0 = const_cast<char *>(args[0]);
-  char *const *typed_args = const_cast<char *const *>(args);
+  char* arg0 = const_cast<char *>(args[0]);
+  char* const* typed_args = const_cast<char* const*>(args); // need to change from const char* to char* const
 
   // check if command is exit
   if (strcmp(arg0, "exit") == 0) {
     exit(0);
   }
 
-  // chek if command is cd
+  // check if command is cd
   if (strcmp(arg0, "cd") == 0) {
     cdCommand(args);
     return;
@@ -217,8 +200,7 @@ void runCommand(const char *args[]) {
   // run commands with absolute and relative paths
   if ((pid = fork()) == 0) { // child
     // first try handle the command through absolute path
-    int c1 = execv(
-        arg0, typed_args); // need to change from const char* to char *const
+    int c1 = execv(arg0, typed_args);
     if (c1 < 0) {          // error
       // second try handle the command through relative path
       char *envptr = getenv("PATH");
@@ -227,15 +209,10 @@ void runCommand(const char *args[]) {
       if (delim != nullptr) {
         bufferSize = strlen(envptr) - strlen(delim);
       }
-      char *newpath = new char[bufferSize + strlen(arg0) +
-                               1]; //+1 for the '/', null termination is already
-                                   //included in arg0
+      char *newpath = new char[bufferSize + strlen(arg0) + 1]; //+1 for the '/', null termination is already included in arg0
       // copy characters into new path
       snprintf(newpath, bufferSize + 1, "%.*s", (int)bufferSize, envptr);
-      sprintf(
-          newpath + bufferSize, "/%s",
-          arg0); // unix treats multiple backslash ex: // as one / so /ls is a
-                 // valid relative path and would become //ls and still work
+      sprintf(newpath + bufferSize, "/%s",arg0); // unix treats multiple backslash as one so /ls is a valid path and would become //ls and still work
       int c2 = execv(newpath, typed_args);
       if (c2 < 0) { // error
         perror("error: invalid input");
